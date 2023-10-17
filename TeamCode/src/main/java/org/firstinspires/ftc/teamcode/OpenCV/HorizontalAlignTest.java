862a16b9a737291a8090089d590174bd16a69f9c
Raven498 <78182016+Raven498@users.noreply.github.com>
2023-10-17 16:52:38 -0400

    Test program for simulating backdrop horizontal align

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/HorizontalAlignTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/HorizontalAlignTest.java
@@ -1,1 +1,69 @@
+package org.firstinspires.ftc.teamcode.OpenCV;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@TeleOp(name = "HorizontalAlignTest")
+public class HorizontalAlignTest extends LinearOpMode {
+    private HWMap hwMap;
+    private AprilTagProcessor tagProcessor;
+    private VisionPortal visionPortal;
+    private DcMotorEx leftFrontMotor;
+    private DcMotorEx rightFrontMotor;
+    private DcMotorEx rightBackMotor;
+    private DcMotorEx leftBackMotor;
+  
+    @Override
+    public void runOpMode() throws InterruptedException {
+        hwMap = new HWMap(telemetry, hardwareMap);
+        tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
+
+        waitForStart();
+
+        drive(0.5);
+        while (opModeIsActive()) {
+            if (tagProcessor.getDetections().size() > 0) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+                tele(tag);
+                drive(0.0);
+            }
+        }
+    }
+
+    public void drive(double power){
+        leftFrontMotor.setPower(power);
+        rightFrontMotor.setPower(power);
+        rightBackMotor.setPower(power);
+        leftBackMotor.setPower(power);
+    }
+
+    public void tele(AprilTagDetection tag){
+        telemetry.addData("-", tag.id);
+        telemetry.addData("x", tag.ftcPose.x);
+        telemetry.addData("y", tag.ftcPose.y);
+        telemetry.addData("z", tag.ftcPose.z);
+        telemetry.addData("roll", tag.ftcPose.roll);
+        telemetry.addData("pitch", tag.ftcPose.pitch);
+        telemetry.addData("yaw", tag.ftcPose.yaw);
+        telemetry.addData("-", "outside");
+        telemetry.update();
+    }
+}
