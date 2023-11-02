6a20bc6dc05156e31014e19cdf4216eeb48d28ec
OmkarRobotics <122411633+OmkarRobotics@users.noreply.github.com>
2023-11-02 17:22:43 -0400

    Still Debugging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cycle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cycle.java
@@ -39,47 +39,56 @@ public class Cycle {
 
     }
 
+
     public void loop() {
-        telemetry.addLine("In cycle");
-        telemetry.update();
-        while (true) {
+        telemetry.addData("In cycle",1);
+            telemetry.addData("in while loop in cycle",1);
             switch (state) {
                 case start:
-
+                    telemetry.addData("in start in cycle",1);
                     if (gamepad.a) {
+                        telemetry.addData("a pressed in cycle",1);
                         state = cycleFSM.intake;
                     }
                     if (gamepad.options) {
+                        telemetry.addData("options pressed in cycle",1);
                         state = cycleFSM.ejection;
                     }
                     if (gamepad.y) {
+                        telemetry.addData("y pressed in cycle",1);
                         state = cycleFSM.transfer;
                     }
                     if (gamepad.left_bumper) {
+                        telemetry.addData("left_bumper pressed in cycle",1);
                         state = cycleFSM.outtake;
                     }
                     break;
                 case intake:
-                        intakeMotor.setPower(0.5);
+                    telemetry.addData("In intake",1);
+                    intakeMotor.setPower(0.5);
+                    if(gamepad.x)
                         state = cycleFSM.start;
                    break;
                 case ejection:
-                        intakeMotor.setPower(-0.5);
+                    telemetry.addData("In ejection",1);
+                    intakeMotor.setPower(-0.5);
                     state = cycleFSM.start;
                     break;
                 case transfer:
-                        linearSlidesLeft.setPower(0.5);
+                    telemetry.addData("In transfer",1);
+                    linearSlidesLeft.setPower(0.5);
                         linearSlidesRight.setPower(0.5);
                     state = cycleFSM.start;
                     break;
                 case outtake:
-                        outakeServoLeft.setPosition(0.75);
+                    telemetry.addData("In outtake",1);
+                    outakeServoLeft.setPosition(0.75);
                         outakeServoRight.setPosition(0.75);
                     state = cycleFSM.start;
-                    return;
-            }
-        }
 
+            }
+        telemetry.update();
+        }
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMain.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -23,6 +25,7 @@ public class TeleopMain extends OpMode {
     DroneLaunch drone;
     Hanging hang;
     FieldCentricDrive drive;
+    Failsafe failsafe;
 
     @Override
     public void init() { //works
@@ -33,49 +36,61 @@ public class TeleopMain extends OpMode {
         drone = new DroneLaunch(hardware);
         hang = new Hanging(hardware);
         drive = new FieldCentricDrive(telemetry, hardwareMap);
+        failsafe = new Failsafe(hardware);
         telemetry.addData("-", "Init Done");
         telemetry.update();
     }
 
-        @Override
-        public void loop () {
+    @Override
+    public void loop() {
+        boolean inLoop = true;
             switch (state) { //exit state?
                 case start:
-                    telemetry.addLine("in start");
-                    telemetry.update();
+                    telemetry.addData("in start",1);
                     if (gamepad1.x) {
-                        telemetry.addLine("X pressed");
-                        telemetry.update();
+                        telemetry.addData("x pressed",1);
                         state = RobotFSM.cycleFSM;
                     }
                     if (gamepad1.y) {
                         state = RobotFSM.hangingFSM;
+                        telemetry.addData("y pressed", 1);
                     }
                     if (gamepad1.a) {
                         state = RobotFSM.droneFSM;
+                        telemetry.addData("a pressed", 1);
                     }
                     if (gamepad1.b) {
                         state = RobotFSM.failsafe;
+                        telemetry.addData("b pressed", 1);
                     }
                     break;
+
                 case cycleFSM:
+                    telemetry.addData("in cycle state", 1);
                     cycle.loop();
                     state = RobotFSM.start;
                     break;
                 case hangingFSM:
+                    telemetry.addData("in hanging state", 1);
                     hang.loop();
                     state = RobotFSM.start;
                     break;
                 case droneFSM:
+                    telemetry.addData("in drone state", 1);
                     drone.loop();
                     state = RobotFSM.start;
                     break;
                 case failsafe:
+                    telemetry.addData("in failsafe state", 1);
+                    inLoop = false;
                     state = RobotFSM.start;
                     break;
             }
-            telemetry.addLine("end of Main loop");
-            telemetry.update();
-            //drivetrain code here
-        }
+
+
+        telemetry.addData("end of Main loop", 1);
+        telemetry.update();
+        //drivetrain code here
+    }
+
 }
