8ed160adcacd739b3882ebb2bf29aa8e8205f70b
Vivaan S <72049995+Vivaan-crypto@users.noreply.github.com>
2023-11-03 22:56:45 -0400

    -

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensor.java
@@ -27,7 +27,28 @@ public class colorSensor extends LinearOpMode {
         }
         waitForStart();
         while(opModeIsActive()){
+            telemetry.addData("Red-1: ", csLeft.red());
+            telemetry.addData("Blue-1: ", csLeft.blue());
+            telemetry.addData("Green-1: ", csLeft.green());
+            telemetry.addData("Red-2: ", csRight.red());
+            telemetry.addData("Blue-2: ", csRight.blue());
+            telemetry.addData("Green-2: ", csRight.green());
+            telemetry.update();
+            if(controller.a1){
 
+            }
+
+            //CS1
+            //Purple Pixel: R- G- B-
+            //Yellow Pixel: R- G- B-
+            //Green Pixel: R- G- B-
+            //White Pixel: R- G- B-
+
+            //CS2
+            //Purple Pixel: R- G- B-
+            //Yellow Pixel: R- G- B-
+            //Green Pixel: R- G- B-
+            //White Pixel: R- G- B-
         }
     }
 }
