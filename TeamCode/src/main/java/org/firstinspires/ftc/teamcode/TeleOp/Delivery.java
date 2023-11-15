f94b8698b41495d6e3194fa1a1e4e7447c78489b
Raven498 <78182016+Raven498@users.noreply.github.com>
2023-11-15 11:10:24 -0500

    start delivery procedure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Delivery.java
@@ -0,0 +1,40 @@
+/*
+Test OpMode for developing the following features:
+1. Slide-Arm Automation: Ensuring arm only moves once slide is of clearing height (going to + coming from deposit pos)
+2. Slide Height Indexing
+3. Arm Alignment with backdrop
+4. Dropoff of pixels
+
+Integrates all delivery mechanisms into one procedure with the following steps:
+1. Slide goes up + arm moves to deposit pos (slide-arm automation occurs here)
+2. Height selected - driver controlled
+3. Arm aligns with backdrop + drops off pixels
+4. Arm goes back to intake pos + slides go down (slide-arm automation occurs here)
+ */
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.dashboard.FtcDashboard;
+
+@Config
+@TeleOp
+public class Delivery extends LinearOpMode {
+    //Constants for facilitating arm + slide movement -> need configuration
+    public static double clearingHeight = 100; //slide height at which arm is allowed to move to deposit pos
+    public static double rowIncrement = 10; //height increment for each row on backdrop
+
+    private HWMap hwMap;
+    private Arm arm;
+    private Claws claws;
+    private Slides slides;
+    
+    @Override
+    public void runOpMode(){
+        while(opModeIsActive()){
+            hwMap = new HWMap(telemetry, hardwareMap);
+            
+        }
+    }
+}
