2ad3417a40e148afa5a505f09c0de6e19abd455e
Raven498 <ramskashyap@gmail.com>
2023-11-25 18:27:47 -0500

    Changed Version names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FSMs/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FSMs/MainTeleOp.java
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.IntakeController;
 import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.Slides;
 import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.TransferController;
 
-@TeleOp(name = "TeleOp")
+@TeleOp(name = "TeleOp-1.0.3")
 public class MainTeleOp extends LinearOpMode {
 
     public enum RobotFSM {
