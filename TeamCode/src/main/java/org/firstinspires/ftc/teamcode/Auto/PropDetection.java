3d2c24a1cdaea6052f17cd640338ace0a66f52e4
Nikilesh <78182016+Raven498@users.noreply.github.com>
2023-12-26 14:38:38 -0500

    move current prop detect code to this branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/PropDetection.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.OpenCV;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.OpenCV.Detector;
+
+import java.util.ArrayList;
+
+@Autonomous
+public class PropDetection extends LinearOpMode {
+    Detector detector;
+    ArrayList<Double> contourAreas;
+    @Override
+    public void runOpMode(){
+        telemetry.addData("-", "INIT START");
+        telemetry.update();
+        detector = new Detector(hardwareMap, telemetry);
+        detector.detect();
+        waitForStart();
+        contourAreas = detector.getContourAreas();
+        while(opModeIsActive()) {
+            telemetry.addData("FIND CONTOUR NUM: ", detector.getFindContourNum());
+            telemetry.addData("FILTER CONTOUR NUM: ", detector.getFilterContourNum());
+            telemetry.addData("POSITION: ", detector.getPosition());
+            telemetry.addData("x", detector.getX());
+            telemetry.addData("y", detector.getY());
+            telemetry.addData("contour areas: ", contourAreas);
+            telemetry.addData("redMax: ", detector.getRedMax());
+            telemetry.update();
+        }
+    }
+}
